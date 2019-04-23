d04283ce771d4bc5f058fe3a001b8e02e11fabff
kobatakuJP <36495393+kobatakuJP@users.noreply.github.com>
2019-04-23 18:18:43 +0900

    layout

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,10 @@
 <template>
-  <div class="hello">
-    <div class="contdown">{{msg}}</div>
+  <div id="wrap">
+    <div id="head">令和まで</div>
+    <div id="strong-area">
+      <span class="dgtl">{{strong}}</span>
+    </div>
+    <div id="detail-area" class="dgtl">{{detail}}</div>
   </div>
 </template>
 
@@ -11,29 +15,62 @@ export default {
   name: 'HelloWorld',
   data() {
     return {
-      msg: 0
+      diff: 0,
     }
   },
   methods: {
     updateReal: function () {
       setInterval(() => {
-        this.msg = moment(moment([2019, 5, 1]).diff(moment())).format("D HH:mm:ss");
+        this.diff = moment([2019, 5, 1]).diff(moment());
       }, 500)
     }
   },
   mounted: function () {
     this.updateReal()
+  },
+  computed: {
+    detail: function () {
+      return moment(this.diff).format("D  HH : mm : ss")
+    },
+    strong: function () {
+      if (this.diff > 1000 * 60 * 60 * 24) {
+        // 1日以上なら日付を強調
+        return `${moment(this.diff).format("D")} 日`
+      } else if (this.diff > 1000 * 60 * 60) {
+        // 1時間以上なら時間を強調
+        return `${moment(this.diff).format("h")} 時間`
+      } else if (this.diff > 1000 * 60) {
+        // 1分以上なら分を強調
+        return `${moment(this.diff).format("m")} 分`
+      } else if (this.diff > 1000) {
+        // 1秒以上なら秒を強調
+        return `${moment(this.diff).format("s")}`
+      }
+    }
   }
 }
 </script>
 
 <style scoped>
-.contdown {
+#wrap {
+  background-color: black;
+  width: 100%;
+  height: 100%;
+}
+#head {
+  color: white;
+}
+#strong-area {
+  font-size: 10em;
+}
+#detail-area {
+  font-size: 2em;
+}
+.dgtl {
   font-family: "DSEG14-Classic";
   font-weight: bold;
   font-style: italic;
   color: #f2f804;
-  text-shadow: 0px 0px 3px #f2f804;
-  background-color: black;
+  text-shadow: 0px 0px 0.2em #f2f804;
 }
 </style>
